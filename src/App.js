7ac1e7965a3d27b59a7254037b42106bedd54602
Daniel Ochoa <docho008@gmail.com>
2020-01-24 16:04:35 -0500

    this is the second commit. Added background image and IP

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import backgroundImage from './img/backgroundImage.jpeg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* URL To Webpage: */}
+      {/*  ec2-18-232-64-251.compute-1.amazonaws.com  */}
+     
+      <h1>My IP address: 18.232.64.251</h1>
+      <img className="body" src={backgroundImage} alt=""/>
+
+
+
     </div>
   );
 }
