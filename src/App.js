22d3675b8f921415241d5501479e2f6cf9a9bef7
EC2 Default User <ec2-user@ip-172-31-91-61.ec2.internal>
2020-01-24 23:34:16 +0000

    edited App.js to display the correct IP address and edited url comment for the correct link to app

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
   return (
     <div className="App">
 
-      {/* URL To Webpage: */}
-      {/*  ec2-18-232-64-251.compute-1.amazonaws.com  */}
+      {/*    URL To Webpage:    */}
+      {/*    http://ec2-3-91-230-44.compute-1.amazonaws.com/cloud-computing-a2/could-computing-a2/build/    */}
      
-      <h1>My IP address: 18.232.64.251</h1>
+      <h1>My IP address: 3.91.230.44</h1>
       <img className="body" src={backgroundImage} alt=""/>
 
 
